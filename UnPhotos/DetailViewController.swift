5cea920e15d078aade3ea19e8efe7446d0811d25
Nada Yehia Dawoud <nadawoud@gmail.com>
2019-11-18 23:39:16 +0200

    Add DetialViewController

---
+++ UnPhotos/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  UnPhotos
+//
+//  Created by Nada Yehia Dawoud on 11/18/19.
+//  Copyright © 2019 Mobile Apps Kitchen. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
