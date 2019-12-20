f0d9617c68f6c0eae64dc5fbc3e2dee8ee975860
Nada Yehia Dawoud <nadawoud@gmail.com>
2019-12-21 00:03:20 +0200

    Move UnsplashEndpoint to a new file

--- UnPhotos/Networking/Endpoint.swift
+++ UnPhotos/Networking/Endpoint.swift
@@ -27,33 +27,3 @@ extension Endpoint {
         return URLRequest(url: urlComponent.url!)
     }
 }
-
-enum Order: String {
-    case popular, latest, oldest
-}
-
-enum UnspashEndpoint: Endpoint {
-    case photos(id: String, order: Order)
-
-    var baseUrl: String {
-        return UnsplashClient.baseUrl
-    }
-
-    var path: String {
-        switch self {
-        case .photos:
-            return "/photos"
-        }
-    }
-
-    var urlParameters: [URLQueryItem] {
-        switch self {
-        case .photos(let id, let order):
-            return [
-                URLQueryItem(name: "client_id", value: id),
-                URLQueryItem(name: "order_by", value: order.rawValue),
-                URLQueryItem(name: "per_page", value: "30")
-            ]
-        }
-    }
-}
